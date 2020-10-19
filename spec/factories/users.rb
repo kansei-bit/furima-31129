7d4bc0b759fa069d9b498369099fc5d9a5d5defa
kansei-bit <kansei1001@gmail.com>
2020-10-19 17:53:59 +0900

    Gem RuboCopによる自動修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password { password }
+    password_confirmation { password }
     gimei = Gimei.name
-    lname {gimei.last.kanji}
-    fname {gimei.first.kanji}
-    lname_katakana {gimei.last.katakana}
-    fname_katakana {gimei.first.katakana}
-    birth_date {Faker::Date.birthday(min_age: 5, max_age: 90)}
+    lname { gimei.last.kanji }
+    fname { gimei.first.kanji }
+    lname_katakana { gimei.last.katakana }
+    fname_katakana { gimei.first.katakana }
+    birth_date { Faker::Date.birthday(min_age: 5, max_age: 90) }
   end
 end
