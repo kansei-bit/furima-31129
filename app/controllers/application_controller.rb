93e92a3d03689184fc4ea7929ee3595dfd4d4690
kansei-bit <kansei1001@gmail.com>
2020-10-19 11:56:33 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    binding.pry
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :lname, :fname, :lname_katakana, :fname_katakana, :birth_date])
+  end
 end
--- db/migrate/20201016135657_devise_create_users.rb
+++ db/migrate/20201016135657_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      @PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: "", with: @PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+      t.string :nickname, null: false, default: ""
+      t.string :lname, null: false, default: ""
+      t.string :fname, null: false, default: ""
+      t.string :lname_katakana, null: false, default: "", format: { with: /^[ァ-ンヴー]*$/, message: '全角文字を使用してください' } 
+      t.string :fname_katakana, null: false, default: "", format: { with: /^[ァ-ンヴー]*$/, message: '全角文字を使用してください' } 
+      t.date :birth_date, null: false
 
       ## Recoverable
       t.string   :reset_password_token
