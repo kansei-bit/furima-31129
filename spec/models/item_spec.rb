3802ccf40db1c99078813a47c09b31f93002bf41
kansei-bit <kansei1001@gmail.com>
2020-10-24 00:23:09 +0900

    itemモデルの単体テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,43 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
   describe '商品出品機能' do
     context '出品がうまくいくとき' do
-      it "nameとdescriptionとimageとpriceが存在すれば登録できる" do
-      end
-      it "category_idとstatus_idとdelivery_charge_idとprefecture_idとshipping_date_idのidが1以外なら登録できる" do
-      end
-      it "priceが300円以上9999999円以下なら登録できる" do
-      end
-      it "ユーザーが紐づいていると登録できる" do
-      end
+      it "nameとdescriptionとimageとpriceが存在し、category_idとstatus_idとdelivery_charge_idとprefecture_idとshipping_date_idのidが1以外で、ユーザーが紐づいていて、priceが300円以上9999999円以下なら登録できる" do
+        expect(@item).to be_valid
       end
     end
 
     context '出品がうまくいかないとき' do
       it "nameがなければ登録できない" do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it "descriptionがなければ登録できない" do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it "imageがなければ登録できない" do
+        @item.image = nil
+        @item.valid?
+        binding.pry
+        # expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it "priceがなければ登録できない" do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "category_idが1ならば登録できない" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it "status_idが1ならば登録できない" do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it "delivery_charge_idが1ならば登録できない" do
+        @item.delivery_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge must be other than 1")
       end
       it "prefecture_idが1ならば登録できない" do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it "shipping_date_idが1ならば登録できない" do
+        @item.shipping_date_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping date must be other than 1")
       end
       it "priceが300円未満ならば登録できない" do
+        @item.price = 200
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it "priceが9999999円より大きければ登録できない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it "ユーザーが紐づいていなければ登録できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
